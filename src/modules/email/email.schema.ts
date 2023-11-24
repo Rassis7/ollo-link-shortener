f7eb505c0eff91b811f506b3c33dd657e4530869
Romulo Assis <romulo.assis@ifood.com.br>
2023-11-24 09:38:02 -0300

    feat: 🎸 create email module

---
+++ src/modules/email/email.schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const emailCore = z.object({
+  fromEmail: z.string().email(),
+  fromName: z.string(),
+  recipients: z
+    .object({
+      name: z.string().nullable(),
+      email: z.string().email(),
+      variables: z
+        .object({
+          variable: z.string(),
+          value: z.string(),
+        })
+        .array(),
+    })
+    .array(),
+  subject: z.string(),
+  templateId: z.string().nullable(),
+  htmlTemplate: z.string().nullable(),
+});
+
+export type SendEmailProps = z.infer<typeof emailCore>;
+
+export type EmailHandlerParams = Omit<
+  SendEmailProps,
+  "fromEmail" | "fromName" | "templateId" | "htmlTemplate"
+>;
