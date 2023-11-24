2064a58c551521ccd03303366ea84dd013f60565
Romulo Assis <romulo.assis@ifood.com.br>
2023-11-24 09:38:15 -0300

    feat: 🎸 send email when create user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { createUser, findUsers } from "./user.service";
 import { CreateUserInput } from "./user.schema";
+import { sendVerifyEmailHandler } from "../email/email.service";
 
 type RegisterUserHandlerRequestProps = FastifyRequest<{
   Body: CreateUserInput;
@@ -12,6 +13,24 @@ export async function registerUserHandler(
 ) {
   const { body } = request;
   const user = await createUser(body);
+
+  // TODO: In future send in queue
+  await sendVerifyEmailHandler({
+    subject: "Confirme Seu Cadastro - Importante!",
+    recipients: [
+      {
+        email: user.email,
+        name: null,
+        variables: [
+          { variable: "expire_in", value: "48" },
+          {
+            variable: "verification_url",
+            value: "https://ollo.li/verification",
+          },
+        ],
+      },
+    ],
+  });
 
   return reply.code(201).send(user);
 }
