575bc32eb4bc3aaf8a1a460103f2f5a7af2b932d
Romulo Assis <romulo.assis@ifood.com.br>
2023-12-16 08:58:22 -0300

    test: 💍 create a tests to helper folder

--- src/__tests__/healthcheck.test.ts
+++ src/__tests__/healthcheck.test.ts
@@ -2,8 +2,6 @@ import supertest from "supertest";
 import { server as app } from "../server";
 
 test("GET `/healthcheck` route", async () => {
-  await app.ready();
-
   const response = await supertest(app.server)
     .get("/healthcheck")
     .expect(200)
