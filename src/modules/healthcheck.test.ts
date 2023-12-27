a6bfd697f4e54a5cb3528daf63b2a05c9d876bc4
Romulo Assis <romulo.assis@ifood.com.br>
2023-12-27 12:00:14 -0300

    test: 💍 use fastify native inject

--- src/modules/healthcheck.test.ts
+++ src/modules/healthcheck.test.ts
@@ -1,11 +1,11 @@
-import supertest from "supertest";
 import { server as app } from "../server";
 
-test("GET `/healthcheck` route", async () => {
-  const response = await supertest(app.server)
-    .get("/healthcheck")
-    .expect(200)
-    .expect("Content-Type", "application/json; charset=utf-8");
+test("GET /healthcheck route", async () => {
+  const response = await app.inject({
+    method: "GET",
+    url: "/healthcheck",
+  });
 
-  expect(response.body).toEqual({ status: "OK" });
+  expect(response.statusCode).toEqual(200);
+  expect(response.json()).toEqual({ status: "OK" });
 });
