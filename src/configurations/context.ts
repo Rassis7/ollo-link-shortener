1a20b98ea897575a45816b845a425a48b3353300
Romulo Assis <romulo.assis@ifood.com.br>
2024-01-04 22:59:06 -0300

    refactor: 💡 move prisma mocks to prisma test file

--- src/configurations/context.ts
+++ src/configurations/context.ts
@@ -1,26 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { mockDeep, DeepMockProxy } from "jest-mock-extended";
 
 export type Context = {
   prisma: PrismaClient;
 };
-
-type MockContext = {
-  prisma: DeepMockProxy<PrismaClient>;
-};
-
-export const createMockContext = (): MockContext => {
-  return {
-    prisma: mockDeep<PrismaClient>(),
-  };
-};
-
-let mockContext: MockContext;
-let context: Context;
-
-beforeEach(() => {
-  mockContext = createMockContext();
-  context = mockContext as unknown as Context;
-});
-
-export { context, mockContext };
--- src/tests/prisma.ts
+++ src/tests/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { mockDeep, mockReset } from "jest-mock-extended";
+import { mockDeep, DeepMockProxy, mockReset } from "jest-mock-extended";
+import { Context } from "@/configurations";
 
 const prismaMock = mockDeep<PrismaClient>();
 
@@ -12,4 +13,22 @@ beforeEach(() => {
   mockReset(prismaMock);
 });
 
-export { prismaMock };
+type MockContext = {
+  prisma: DeepMockProxy<PrismaClient>;
+};
+
+export const createMockContext = (): MockContext => {
+  return {
+    prisma: mockDeep<PrismaClient>(),
+  };
+};
+
+let mockContext: MockContext;
+let context: Context;
+
+beforeEach(() => {
+  mockContext = createMockContext();
+  context = mockContext as unknown as Context;
+});
+
+export { prismaMock, context, mockContext };
