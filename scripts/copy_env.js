f0af9e60caa9a6eaec88426d98c7fbdcd5da542b
Romulo Assis <romulo.assis@ifood.com.br>
2024-02-21 10:12:49 -0300

    feat: 🎸 create seed script

--- scripts/copy_env.js
+++
@@ -1,8 +0,0 @@
-// copy-env.js
-const fs = require("fs");
-const path = require("path");
-
-const source = path.resolve(__dirname, "..", ".env");
-const destination = path.resolve(__dirname, "..", "dist", ".env");
-
-fs.copyFileSync(source, destination);
