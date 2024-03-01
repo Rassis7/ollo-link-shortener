cebf929e3dc9b80dd529e2d2abc7e2cd6a5f612f
Romulo Assis <romulo.assis@ifood.com.br>
2024-03-01 19:12:28 -0300

    feat: 🎸 improve auth configuration

--- src/configurations/auth.ts
+++ src/configurations/auth.ts
@@ -1,6 +1,8 @@
-import fastifyJwt from "@fastify/jwt";
+import fastifyJwt, { FastifyJWT } from "@fastify/jwt";
 import { app as fastify } from "./app";
-import { FastifyRequest } from "fastify";
+import { FastifyReply, FastifyRequest } from "fastify";
+import fastifyCookie from "@fastify/cookie";
+import { sessionHandler } from "@/modules/auth/auth.controller";
 
 fastify.register(fastifyJwt, () => ({
   secret: String(process.env.FASTIFY_JWT_SECRET),
@@ -9,12 +11,30 @@ fastify.register(fastifyJwt, () => ({
   },
   messages: {
     badRequestErrorMessage: "Token informado de maneira incorreta",
-    authorizationTokenExpiredMessage: "Token expirado",
     authorizationTokenUntrusted: "Token não confiável",
     authorizationTokenUnsigned: "Token não assinado",
+    authorizationTokenInvalid: "Token não autorizado",
+    noAuthorizationInHeaderMessage: "Token não enviado",
+    noAuthorizationInCookieMessage: "Token não enviado",
+    badCookieRequestErrorMessage: "Token inválido",
+    authorizationTokenExpiredMessage: "Token expirado",
   },
 }));
 
-fastify.decorate("authenticate", async (request: FastifyRequest) => {
-  await request.jwtVerify();
+fastify.register(fastifyCookie);
+
+export interface FastifyRequestWithCookie extends FastifyRequest {
+  jwt: FastifyJWT;
+  cookies: {
+    access_token: string;
+  };
+}
+
+fastify.after(() => {
+  fastify.decorate(
+    "authenticate",
+    async (request: FastifyRequestWithCookie, reply: FastifyReply) => {
+      await sessionHandler(request, reply);
+    }
+  );
 });
